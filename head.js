117774277f06179879357f30643b42534608ff5c
Suncraft <cookies666@msn.com>
2021-01-05 11:06:38 +0000

    head file and assertions checking

---
+++ head.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👊 👀  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔥🔥🔥 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const head = function(arr) {
+  return arr[0];
+
+};
+
+
+assertEqual(head([]), 5);
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
