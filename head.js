610569e07096099e201a4b06cdacebc1dfcffa0f
Suncraft <cookies666@msn.com>
2021-01-05 11:08:35 +0000

    head file adjustment

--- head.js
+++ head.js
@@ -4,16 +4,14 @@ const assertEqual = function(actual, expected) {
   } else {
     console.log(`🔥🔥🔥 Assertion Failed: ${actual} !== ${expected}`);
   }
-
 };
+
 
 const head = function(arr) {
   return arr[0];
-
 };
 
 
 assertEqual(head([]), 5);
-
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
