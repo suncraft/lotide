8902b585e14b47dad8ce12309978f9805924d4a4
Suncraft <cookies666@msn.com>
2021-01-06 07:35:40 +0000

    Adjustments

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,11 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("👊 👀  " + "Assertion Passed: " + actual + " === " + expected);
+    // console.log("👊 👀  " + "Assertion Passed: " + actual + " === " + expected);
+    console.log(`👊 👀  Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("🔥🔥🔥 " + "Assertion Failed: " + actual + " !== " + expected);
+    // console.log("🔥🔥🔥 " + "Assertion Failed: " + actual + " !== " + expected);
+    console.log(`🔥🔥🔥 Assertion Failed: ${actual} !== ${expected}`);
   }
 
 };
