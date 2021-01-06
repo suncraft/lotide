8968d3f75d9930754b061f84cd180b724488043f
Suncraft <cookies666@msn.com>
2021-01-06 06:19:46 +0000

    new functions checking if arrays are equal

---
+++ eqArrays.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👊 👀  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔥🔥🔥 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  let truthyFalsy;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] === arr2[i]) {
+      truthyFalsy = true;
+    } else {
+      truthyFalsy = false;
+    }
+  }
+  return truthyFalsy;
+};
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);// => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
