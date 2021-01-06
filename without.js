6f984b1e4455b4f0ec930048bcd54b25263a23c5
Suncraft <cookies666@msn.com>
2021-01-06 17:29:31 +0000

    making sure it copies array with spread operator

--- without.js
+++ without.js
@@ -19,7 +19,7 @@ const assertArraysEqual = function(arr1, arr2) {
 };
 
 const without = function(source, itemsToRemove) {
-  let newArr = source;
+  let newArr = [...source];
   // console.log(source);
   for (let i = 0; i < newArr.length; i++) {
     // console.log(newArr[i]);
@@ -30,16 +30,18 @@ const without = function(source, itemsToRemove) {
       } 
     }
   }
-  // console.log(source);
+  console.log("source = " + source);
+  console.log("newArr = " + newArr);
   return newArr;
 };
 
 console.log(without([1, 2, 3], [1])); // => [2, 3]
 console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
-const words = ["hello", "world", "lighthouse"];
+const words = ["hello", "world", "lighthouse~~~"];
 without(words, ["lighthouse"]); // no need to capture return value for this test case
 // Make sure the original array was not altered by the without function
 assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+// console.log(words);
 
 
 
