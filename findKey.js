0edf0c91bc89950ee3d95455e926a77a0486b623
Suncraft <cookies666@msn.com>
2021-01-10 01:55:24 +0000

    findkey function working

---
+++ findKey.js
@@ -0,0 +1,42 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👊 👀  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔥🔥🔥 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(obj, callback) {
+  let keyFound = undefined;
+  for (let key in obj) {
+    if (callback(obj[key]) === true) {
+      keyFound = key;
+      return keyFound;
+      // break;
+    }
+  }
+  return keyFound;
+}
+
+// const findKeyByValue = function(myObj, value) {
+//   for (let key in myObj) {
+//     if (myObj[key] === value) {
+//       return key;
+//     }
+//   }
+// };
+
+let test1 = {
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}
+
+// console.log(findKey(test1, x => x.stars === 2)); // => "noma"
+console.log(assertEqual(findKey(test1, x => x.stars === 2), "noma"));
+
+// console.log(findKey({}, x => x.stars === 3)); // => undefined
+console.log(assertEqual(findKey({}, x => x.stars === 3), undefined));
