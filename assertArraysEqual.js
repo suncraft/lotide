d3850d27f95f0b38379df846ce9517886b2beb63
Suncraft <cookies666@msn.com>
2021-01-11 22:56:06 +0000

    Continuing modularizing lotide

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,22 +1,5 @@
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`👊 👀  Assertion Passed: ${actual} === ${expected}`);
-//   } else {
-//     console.log(`🔥🔥🔥 Assertion Failed: ${actual} !== ${expected}`);
-//   }
-// };
 
-const eqArrays = function(arr1, arr2) {
-  let truthyFalsy;
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] === arr2[i]) {
-      truthyFalsy = true;
-    } else {
-      truthyFalsy = false;
-    }
-  }
-  return truthyFalsy;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2) === true) {
@@ -26,5 +9,4 @@ const assertArraysEqual = function(arr1, arr2) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
+module.exports = assertArraysEqual;
---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,5 @@
+const assertArraysEqual = require('../assertArraysEqual.js');
+
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
