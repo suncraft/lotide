9aa276df1b7bc1d5d9d2705656cbc17a75d43271
Suncraft <cookies666@msn.com>
2021-01-11 22:24:50 +0000

    Added modules to eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👊 👀  Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔥🔥🔥 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+
 
 const eqArrays = function(arr1, arr2) {
   let truthyFalsy;
@@ -19,10 +13,5 @@ const eqArrays = function(arr1, arr2) {
 };
 
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => false
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);// => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+module.exports = eqArrays;
