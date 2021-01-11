e7c6bd9c1ed4a555c4200c8bab5f1583c3350f86
Suncraft <cookies666@msn.com>
2021-01-11 22:16:43 +0000

    refactored and added modules to tail files

--- tail.js
+++ tail.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👊 👀  Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔥🔥🔥 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+
 
 // let newArr = [];
 const tail = function(arr) {
@@ -14,8 +8,4 @@ const tail = function(arr) {
   return console.log(newArr);
 };
 
-
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
